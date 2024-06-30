a696d9a7b8dfd2a496ec0135b8520c5f9d82e0d0
Oleksii Narovskyi <o.narovskyi@gmail.com>
2024-06-30 17:48:39 +0300

    finish homework

---
+++ src/basic/6.ts
@@ -0,0 +1,27 @@
+interface Location {
+    city: string;
+    country: string
+}
+
+interface User {
+    name: string;
+    age: number;
+    email: string;
+    address?: Location
+}
+
+const mango: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
+
+const poly: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com'
+};
---
+++ src/generics/4.ts
@@ -0,0 +1,10 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  // Оновлення користувача
+}
